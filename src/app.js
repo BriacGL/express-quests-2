ab0d25dd5fcbd940a80f045a6b066f6ab42dc169
BriacGL <briac.gauthierlefeuvre@gmail.com>
2024-01-16 13:29:58 +0100

    ok

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-const express = require("express");
+// app.js
 
+const express = require('express');
 const app = express();
+app.use(express.json());
 
-const movieControllers = require("./controllers/movieControllers");
-const userControllers = require("./controllers/userControllers")
+const movieControllers = require('./controllers/movieControllers');
+const userControllers = require('./controllers/userControllers');
 
-app.get("/api/movies", movieControllers.getMovies);
-app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get('/api/movies', movieControllers.getMovies);
+app.get('/api/movies/:id', movieControllers.getMovieById);
 
 // Définir la route GET pour obtenir la liste des utilisateurs
 app.get('/api/users', userControllers.getUsers);
 
 // Définir la route GET pour obtenir un utilisateur par ID
 app.get('/api/users/:id', userControllers.getUserById);
+
+// Définir la route POST pour ajouter un nouveau film
+app.post('/api/movies', (req, res) => {
+    movieControllers.postMovie(req, res);
+});
+
+// Définir la route POST pour ajouter un nouvel utilisateur
+app.post('/api/users', (req, res) => {
+    userControllers.postUser(req, res);
+});
 
 module.exports = app;
